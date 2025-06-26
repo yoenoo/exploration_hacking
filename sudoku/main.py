b91771e4d32995020efa66d6f62550bfa206ab9b
Yeonwoo Jang <yjang385@gmail.com>
2025-06-26 21:16:20 +0000

    add Gemma-3-4B result

--- sudoku/main.py
+++ sudoku/main.py
@@ -1,3 +1,4 @@
+import os
 from collections import defaultdict
 from utils import generate_puzzle, solve, correctly_filled_cells
 
@@ -6,26 +7,33 @@ n_tries = 10
 grid_size = 3
 # difficulties = [0.02, 0.1, 0.2, 0.3, 0.5, 0.9]
 difficulties = [0.02, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+difficulties = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 # 0.02 -> only 1 cell to fill
 # 0.99 -> only 1 cell is filled
 
-"""
-# TODO: 
-- RL simulation using ICL? SFT? already in the prompt the model is taught the rules of sudoku, but not given any examples
-- malign init?
-- rollouts (w/ nucleus sampling)
-- can do you an RL training loop?
-"""
+
+
+
+from sudoku import Sudoku
+from transformers import AutoTokenizer, AutoModelForCausalLM
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-3-4b-it", token=os.getenv("HF_TOKEN"))
+model = AutoModelForCausalLM.from_pretrained("google/gemma-3-4b-it", token=os.getenv("HF_TOKEN"))
 
 
 results = defaultdict(list)
 for difficulty in difficulties:
   print(f"Sudoku Difficulty: {difficulty}")
-  for _ in range(n_tries):
+  # for _ in range(n_tries):
+  while len(results[difficulty]) < n_tries:
     puzzle = generate_puzzle(grid_size, difficulty)
     # answer, correct = solve(puzzle, model="claude-sonnet-4-20250514", max_tokens=4096)
-    answer, correct = solve(puzzle, model="claude-3-5-haiku-20241022", max_tokens=1024)
-    results[difficulty].append({"puzzle": puzzle, "answer": answer, "correct": correct})
+    # answer, correct = solve(puzzle, model="claude-3-5-haiku-20241022", max_tokens=1024)
+    try:
+      answer, correct = solve(puzzle, model, tokenizer, max_tokens=4096)
+      results[difficulty].append({"puzzle": puzzle, "answer": answer, "correct": correct})
+      print(results[difficulty])
+    except SyntaxError:
+      continue
 
   acc = sum(r["correct"] for r in results[difficulty]) / n_tries
   print(f"Accuracy: {acc:.4f}")
