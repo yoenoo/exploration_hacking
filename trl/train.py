5e5027f2243e6da7819f658c2a1b522c482e4423
Yeonwoo Jang <yjang385@gmail.com>
2025-06-26 20:43:31 -0700

    add trl scripts

---
+++ trl/train.py
@@ -0,0 +1,90 @@
+import sys; sys.path.append("sudoku")
+import re
+import ast
+import wandb
+from peft import LoraConfig, get_peft_model
+from transformers import AutoModelForCausalLM
+from trl import GRPOConfig, GRPOTrainer
+from datasets import load_dataset
+from utils import generate_prompt_reasoning
+from sudoku import Sudoku
+
+
+wandb.init(project="sudoku-trl")
+
+
+## dataset
+def generate_prompt(example):
+  board = ast.literal_eval(example["board"])
+  prompt = generate_prompt_reasoning(Sudoku(board=board))
+  return {
+    "prompt": [
+      # {"role": "system", "content": SYSTEM_PROMPT},
+      {"role": "user", "content": prompt},
+    ],
+  }
+
+train_dataset = load_dataset("yoenoo/sudoku", split="train")
+train_dataset = train_dataset.map(generate_prompt)
+
+
+## model
+model_id = "google/gemma-3-4b-it"
+model = AutoModelForCausalLM.from_pretrained(
+  model_id,
+  torch_dtype="auto",
+  device_map="auto",
+)
+
+lora_config = LoraConfig(
+  task_type="CAUSAL_LM",
+  r=8,
+  lora_alpha=32,
+  lora_dropout=0.1,
+  target_modules=["q_proj", "v_proj"],
+)
+
+model = get_peft_model(model, lora_config)
+model.print_trainable_parameters()
+
+
+training_args = GRPOConfig(
+  output_dir="GRPO",
+  learning_rate=2e-5,
+  remove_unused_columns=False, # to access the solution column in accuracy_reward
+  gradient_accumulation_steps=2,
+  num_train_epochs=1,
+  # bf16=True,
+  bf16=False,
+
+  # Parameters that control de data preprocessing
+  max_completion_length=256, # default: 256
+  num_generations=8, # default: 8
+  max_prompt_length=512, # default: 512
+
+  # Parameters related to reporting and saving
+  report_to=["wandb"],
+  logging_steps=1,
+  save_strategy="steps",
+  save_steps=1,
+)
+
+def accuracy_reward(completions, **kwargs):
+  print(completions)
+  exit()
+
+  solution_text = re.findall(r"<solution>(.*?)</solution>", completions, re.DOTALL | re.IGNORECASE)
+  if not solution_text:
+    raise ValueError(f"invalid solution text: {solution_text}") 
+
+  out = ast.literal_eval(solution_text[-1])
+
+
+trainer = GRPOTrainer(
+  model=model,
+  reward_funcs=[accuracy_reward], ## TODO: add format_reward?
+  args=training_args,
+  train_dataset=train_dataset,
+)
+
+trainer.train()
