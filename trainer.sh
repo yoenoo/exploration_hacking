4b3dc81769b2a35c387c1c34d95339cd11a5f251
Yeonwoo Jang <yjang385@gmail.com>
2025-09-05 03:40:37 +0000

    add TOKENIZER_PARALLELISM

--- trainer.sh
+++ trainer.sh
@@ -3,7 +3,7 @@ set -xe
 CUDA_DEVICE_ORDER=PCI_BUS_ID 
 NCCL_ASYNC_ERROR_HANDLING=1 
 
-VLLM_LOGGING_LEVEL=WARNING CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
-  --num_processes=4 \
+TOKENIZERS_PARALLELISM=true VLLM_LOGGING_LEVEL=WARNING CUDA_VISIBLE_DEVICES=0 accelerate launch \
+  --num_processes=1 \
   --module src.train.trainer \
-  --config-name rl_locking_conditional
+  --config-name config_bcb
